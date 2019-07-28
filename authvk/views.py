bd9f67dcbebf9531212747581865b3f686680e4f
hamster197 <hamster197@mail.ru>
2019-07-28 13:53:46 +0300

    1353

--- authvk/views.py
+++ authvk/views.py
@@ -2,6 +2,5 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-#@login_required
 def MainView(request):
     return render(request,'vk/main.html')
