4e7929e5c8e49d2d33ff8cfd2556c95c8aabea86
hamster197 <hamster197@mail.ru>
2019-07-28 16:15:29 +0300

    1615

--- authvk/views.py
+++ authvk/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render
 #@login_required
 def MainView(request):
     # profiles = vk.api.friends.get(user_id='70911700', order='hints')#self.id
-    session = vk.Session()
-    vk_api = vk.API(session)
-    p = vk_api.users.get(user_id=1)
-    return render(request,'vk/main.html',{'t':p})
+    # session = vk.Session()
+    # vk_api = vk.API(session)
+    # p = vk_api.users.get(user_id=1)
+    return render(request,'vk/main.html',){'t':p})
