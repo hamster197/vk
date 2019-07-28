6c4265dcfa7fdc54924613bf51c31dcd9ea9ab39
hamster197 <hamster197@mail.ru>
2019-07-28 14:53:17 +0300

    1415

--- authvk/views.py
+++ authvk/views.py
@@ -2,6 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from vkontakte_users.models import User
+
+
 @login_required
 def MainView(request):
-    return render(request,'vk/main.html')
+    User.remote.fetch(ids=[1, 2, 6])
+    user = User.remote.fetch(ids=[70911700])[0]
+    return render(request,'vk/main.html', {'tu':user})
